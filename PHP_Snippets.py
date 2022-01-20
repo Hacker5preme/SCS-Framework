f695afa15fc7ab6e7bba603f65eb3cee889ec22e
Ron Jost <hacker5preme@protonmail.com>
2022-01-20 22:35:16 +0100

    Add files via upload

---
+++ PHP_Snippets.py
@@ -0,0 +1,11 @@
+# Function file from PHP-Vulnerability scans. Easyier to edit:
+
+def PHP_vulnerabilities(php_file, lines):
+    # Describes all vulnerabilites
+    # Calls other Scanners / Analyzers
+    pass
+
+
+def backtrack_variable_PHP(variable, php_file):
+    pass
+
