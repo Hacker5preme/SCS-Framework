3cd8cd84bb51d3656212cfbb94a67351426b0ded
Ron Jost <hacker5preme@protonmail.com>
2022-01-26 13:32:01 +0100

    Update PHP_Snippets.py

--- PHP_Snippets.py
+++ PHP_Snippets.py
@@ -18,7 +18,7 @@ def scan_open_redirect(php_file, lines):
 	
 	# References PHP WP:
 	wp_ref = 'https://developer.wordpress.org/reference/functions/wp_redirect/'
-	CVE_ref_wp = ''
+	CVE_ref = 'https://nvd.nist.gov/vuln/detail/CVE-2020-18660'
 	
 	# Non WP: PHP Code snippet to redirect to URL
 	string_search_begin = 'header('
