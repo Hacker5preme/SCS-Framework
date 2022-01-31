8702ea80e64e67ff855598b790689a5bc8f1f271
Ron Jost <hacker5preme@protonmail.com>
2022-01-31 20:39:33 +0100

    Version 0.1
    Added Interactive mode

--- SourceCodeScanner-Framework.py
+++ SourceCodeScanner-Framework.py
@@ -1,14 +1,16 @@
 '''
 [@] Author: Ron Jost (Hacker5preme)
 [+] Contributors: 1
-[*] Vulnerable Code snippets: 0
+[*] Vulnerable Code snippets: 1
 '''
+# Version 0.1
 
 import copy
 import os
 import argparse
 from colorama import init, Fore
 from PHP_Snippets import *
+
 
 print('')
 # Banner:
@@ -29,13 +31,16 @@ parser.add_argument("-p", '--path', help="Path to directory which shall be scann
                     type=str)
 parser.add_argument('-v', '--verbosity', help="Verbosity level for variable tracking (show variables which are parameters)",
                     type=int, default=0)
+parser.add_argument('-i', '--interactive', help="Interactive Level for output (Show more details per possible vulnerability)",
+                    type=int, default=1)
 args = parser.parse_args()
 path = args.path
 verbosity = args.verbosity
+interactive = args.interactive
 init(autoreset=True)
 # Code Scanner:
-def scancode(path, verbosity):
-
+def scancode(path, verbosity, interactive):
+    php_discoveries = []
     # PHP Files to scan:
     files_to_scan_php = [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) for f in filenames if os.path.splitext(f)[1] == '.php']
   
@@ -54,15 +59,17 @@ def scancode(path, verbosity):
             else:
                 elements_in_line.append(element)
         file_content.close()
-        php_discoveries = PHP_vulnerabilities(file_search, lines, file, verbosity)
-        if type(php_discoveries) != list:
-                php_discoveries = []
+        if len(php_discoveries) == 0:
+            php_discoveries = PHP_vulnerabilities(file_search, lines, file, verbosity)
+        else:
+            php_discoveries = php_discoveries + PHP_vulnerabilities(file_search, lines, file, verbosity)
 
-    Output(php_discoveries)
+    Output(php_discoveries, interactive)
     
 # Output:
-def Output(vulns):
+def Output(vulns, interactive):
     # Output vulnerability finds:
+
     print('Scanning: ' + path + ':')
     print('')
     color = Fore.RED
@@ -71,13 +78,36 @@ def Output(vulns):
         print(color + '[' + str(i) + ']' + ' Possible ' + vuln[0] + ':')
         print(color + str(vuln[1]) + ':' + str(vuln[2]))
         print(Fore.YELLOW + vuln[3])
-        print('References:')
-        for refs in vuln[4]:
-            print(' - ' + refs)
-        print('')
         print('')
         i = i +1
-    # Further User Interaction possible:
+    if interactive == 1:
+        details = input('Enter vulnerability ID to display more Information or exit: ')
+        while details != 'exit':
+            print('')
+            vulnerability = vulns[int(details)]
+            print(color + '[!]' + ' Possible ' + vulnerability[0] + ':')
+            print('')
+            print(color + str(vulnerability[1]) + ':' + str(vulnerability[2]))
+            print(Fore.YELLOW + vulnerability[3])
+            print('')
+            if type(vulnerability[5]) == int:
+                print('References: ')
+                for ref in vulnerability[4]:
+                    print(' - ' + ref)
+                print('')
+            else:
+                variable_info = vulnerability[5]
+                # Check if it is a function or straight variable definition
+                print(Fore.BLUE + '[i] Variable Definition: ' )
+                print(Fore.BLUE + str(vulnerability[1]) + ':' + str(variable_info[0][0]))
+                print(Fore.BLUE + variable_info[0][1])
+                print('')
+                print('References: ')
+                for ref in vulnerability[4]:
+                    print(' - ' + ref)
+                print('')
+
+            details = input('Enter vulnerability ID to display more Information or exit: ')
 
 
-scancode(path, verbosity)
+scancode(path, verbosity, interactive)
